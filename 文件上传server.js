5ac85f16e36cb6f9a1ebdfddd87eaf84b1a242cc
shuangziye <18401484832@163.com>
2019-05-29 18:01:37 +0800

    提交websocket代码

---
+++ 文件上传server.js
@@ -0,0 +1,16 @@
+var WebSocketServer = require('ws');
+var wss = new WebSocketServer.Server({
+  port: 9088
+});
+
+
+
+//有人访问这个接口就会调用
+wss.on('connection', function connection(ws, req) {
+  // 客户端调用触发事件
+  ws.on('message', function incoming(message) {
+    wss.clients.forEach((client) => {
+      client.send(message)
+    })
+  });
+});
