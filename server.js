0c5831947b4a1d99a46f5ad6df0992348738dabe
shuangziye <18401484832@163.com>
2019-05-29 15:13:35 +0800

    websocket通信提交

--- server.js
+++ server.js
@@ -1,55 +1,48 @@
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({
+var WebSocketServer = require('ws');
+var wss = new WebSocketServer.Server({
   port: 9088
 });
 
-// function heartbeat() {
-//   this.isAlive = true;
-// }
+function heartbeat() {
+  this.isAlive = true;
+}
 
-// function noop() {}
 //有人访问这个接口就会调用
 wss.on('connection', function connection(ws, req) {
-  // ws.isAlive = true;
-  // ws.on('pong', heartbeat);
-  //在线的所有客户端
-  var iparr = req.connection.remoteAddress.split(":");
-  ip = iparr[iparr.length - 1]
-  ws.ip = ip
-  wss.clients.forEach((client) => {
-    var ipsend = {
-      status: 0,
-      message: ip + "进入了直播间",
-      name: ''
-    }
-    client.send(JSON.stringify(ipsend))
-  })
+  ws.isAlive = true;
+  ws.on('pong', heartbeat);
   // 客户端调用触发事件
   ws.on('message', function incoming(message) {
+    ws.username = JSON.parse(message).username
     wss.clients.forEach((client) => {
       client.send(message)
     })
   });
 });
-// const interval = setInterval(function ping() {
-//   var arr=[]
-//   var index=0
-//   wss.clients.forEach(function each(ws) {
-//     if (ws.isAlive === false) {
-//       arr.push(ws.ip)
-//       index=1
-//       ws.terminate();
-//     }
-//     ws.isAlive = false;
-//     ws.ping(noop);
-//   });
-//   if(index==1){
-//     wss.clients.forEach(function each(ws) {
-//       ws.send(JSON.stringify({
-//         status: 0,
-//         message: arr[0] + "离开了直播间",
-//         name: ''
-//       }))
-//     })
-//   }
-// }, 15000);
+
+// 定时循环判断是不是前端还在运行
+const interval = setInterval(function ping() {
+  var arr = []
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) {
+      arr.push(ws.username)
+      // 关闭这个通信
+      ws.terminate();
+    }
+    ws.isAlive = false;
+    // 触发pong事件
+    ws.ping();
+  });
+  // 离开的用户给每一个剩余的用户通知一下
+  if (arr.length > 0) {
+    arr.forEach(el => {
+      wss.clients.forEach(function each(ws) {
+        ws.send(JSON.stringify({
+          status: 0,
+          message: el + "离开了直播间",
+          name: ''
+        }))
+      })
+    })
+  }
+}, 15000);
